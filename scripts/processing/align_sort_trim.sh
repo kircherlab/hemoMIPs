70b4c880543d103a7e4a570de2ebd0c919ec45df
kleinertp <38033071+kleinertp@users.noreply.github.com>
2019-01-08 11:31:49 +0100

    Delete align_sort_trim.sh
    unused files deleted

--- scripts/processing/align_sort_trim.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bwa mem -L 80 -M -C /fast/projects/cubit/current/static_data/precomputed/BWA/0.7.12/GRCh37/g1k_phase2/hs37d5.fa <( samtools view -F 513 by_sample/${1}.bam | awk 'BEGIN{ OFS="\n"; FS="\t" }{ print "@"$1"\t"$12"\t"$13"\t"$14,$10,"+",$11 }' ) | samtools view -u - | samtools sort - | /fast/groups/ag_kircher/hemophilia/scripts/pipeline2.0/TrimMIParms.py -d hemomips_design.txt -p | samtools reheader new_header.sam - | samtools sort -O bam -o aligned/${1}.bam -
-samtools index aligned/${1}.bam
